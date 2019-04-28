dce3285d01834f35f488ae32262c6b24dd2d7271
Des Whelan <wheland6@tcd.ie>
2019-04-28 18:51:47 +1200

    add shell SearchArtists component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-// import ErrorMessage from './ErrorMessage'
-// import LoadSubreddit from './LoadSubreddit'
-// import SubredditList from './SubredditList'
-// import WaitIndicator from './WaitIndicator'
+import SearchArtists from './SearchArtists'
 
 const App = () => (
   <div className='app'>
-  
+    <SearchArtists />
   </div>
 )
 
---
+++ client/components/SearchArtists.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Component} from 'react'
+
+class SearchArtists extends React.Component {
+    constructor (props){
+        super(props)
+    }
+
+    render () {
+        return (
+            <h1>Search for artists here!</h1>
+        )
+    }
+}
+
+export default SearchArtists
