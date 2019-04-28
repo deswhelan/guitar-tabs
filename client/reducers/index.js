12ec8b852949791434e1687f8af245c01d419a2d
Des Whelan <wheland6@tcd.ie>
2019-04-28 18:47:52 +1200

    comment out redundant boilerplate code

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,11 @@
 import {combineReducers} from 'redux'
 
 import errorMessage from './error-message'
-import subreddits from './subreddits'
-import waiting from './waiting'
+// import subreddits from './subreddits'
+// import waiting from './waiting'
 
 export default combineReducers({
   errorMessage,
-  subreddits,
-  waiting
+  // subreddits,
+  // waiting
 })
