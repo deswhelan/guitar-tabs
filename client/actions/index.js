f37f05454f17cd61138168b1d48bbe6a832b7f3d
Des Whelan <wheland6@tcd.ie>
2019-05-01 13:49:40 +1200

    refactor out boilerplate

--- client/actions/index.js
+++ client/actions/index.js
@@ -5,8 +5,6 @@ export const SHOW_ERROR = 'SHOW_ERROR'
 export const SET_ARTIST = 'SET_ARTIST'
 export const SET_FAVOURITES = 'SET_FAVOURITES'
 
-import {getFavourites} from '../api'
-
 export const showError = (errorMessage) => {
   return {
     type: SHOW_ERROR,
@@ -59,38 +57,3 @@ export const setFavourites = (favourites) => {
     favourites
   }
 }
-
-
-
-// export const SHOW_ERROR = 'SHOW_ERROR'
-// export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-// export const REQUEST_POSTS = 'REQUEST_POSTS'
-
-// export const requestPosts = () => {
-//   return {
-//     type: REQUEST_POSTS
-//   }
-// }
-
-// export const receivePosts = (posts) => {
-//   return {
-//     type: RECEIVE_POSTS,
-//     posts: posts.map(post => post.data)
-//   }
-// }
-
-
-
-// export function fetchPosts (subreddit) {
-//   return (dispatch) => {
-//     dispatch(requestPosts())
-//     return request
-//       .get(`/api/v1/reddit/subreddit/${subreddit}`)
-//       .then(res => {
-//         dispatch(receivePosts(res.body))
-//       })
-//       .catch(err => {
-//         dispatch(showError(err.message))
-//       })
-//   }
-// }
--- server/favourites.js
+++ server/favourites.js
@@ -8,20 +8,9 @@ router.use(express.json())
 
 router.post('/:songName/:artist/:id', (req, res) => {
     db.addNewFavourite(req.params)
-    
-//   request
-//     .get(`http://www.reddit.com/r/${req.params.subreddit}.json`)
-//     .end((err, result) => {
-//       if (err) {
-//         res.status(500).send(err.message)
-//       } else {
-//         res.json(result.body.data.children)
-//       }
-//     })
 })
 
 router.get('/fetch', (req, res) => {
- 
     return db.listFavourites()
     .then(favourites => res.json(favourites))
 })
