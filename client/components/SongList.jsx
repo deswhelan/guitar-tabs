abe3d5552df94fe0ffb8121ab55953d1697beff2
Des Whelan <wheland6@tcd.ie>
2019-05-01 19:33:38 +1200

    render favs and songs when added/deleted

--- client/components/SongList.jsx
+++ client/components/SongList.jsx
@@ -2,34 +2,38 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 import {addToFavourites} from '../api'
+import {fetchFavourites} from '../actions'
 
-
-const Songlist = ({songList}) => (
+class Songlist extends React.Component{
   
-  <div id="songlist">
-    <ul>
-      {songList.map(song => {
-        let {title, artist, id} = song
-        let url = `https://www.songsterr.com/a/wa/song?id=${id}`
+  handleClick (e) {
+    let songName = e.target.getAttribute('songname')
+    let artist = e.target.getAttribute('artist')
+    let id = e.target.getAttribute('id')
+  
+    addToFavourites(songName, artist, id)
+    .then(this.props.goFetchFavourites())
+  }
 
-        return (
-          <li key={id}>
-            <a target="_blank" href={url}>{title}, </a>
-            <a target="_blank" href={`http://www.songsterr.com/a/wa/artist?id=${artist.id}`}>{artist.name}</a>
-            <button method="post" songname={title} artist={artist.name} id={id} onClick={(e) => handleClick(e)}>Add to favourites</button>
-          </li>
-        )
-      })}
-    </ul>
-  </div>
-)
-
-function handleClick (e) {
-  let songName = e.target.getAttribute('songname')
-  let artist = e.target.getAttribute('artist')
-  let id = e.target.getAttribute('id')
-
-  addToFavourites(songName, artist, id)
+  render () {
+    return (
+      <div id="songlist">
+        <ul>
+          {this.props.songList.map(song => {
+            let {title, artist, id} = song
+            let url = `https://www.songsterr.com/a/wa/song?id=${id}`
+            return (
+              <li key={id}>
+                <a target="_blank" href={url}>{title}, </a>
+                <a target="_blank" href={`http://www.songsterr.com/a/wa/artist?id=${artist.id}`}>{artist.name}</a>
+                <button method="post" songname={title} artist={artist.name} id={id} onClick={(e) => this.handleClick(e)}>Add to favourites</button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
@@ -38,4 +42,11 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Songlist)
+const mapDispatchToProps = dispatch => {
+  return {
+      goFetchFavourites: () => dispatch(fetchFavourites()),
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Songlist)
